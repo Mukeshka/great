ab9e88ec0e3a43683b8a796b9e5e78671c029703
sher108 <88870961+sher108@users.noreply.github.com>
2021-08-31 10:41:43 +0530

    Update rtd.sh

--- rtd.sh
+++ rtd.sh
@@ -3,6 +3,6 @@ pkg install git
 pkg install python 
 pkg install figlet
 rm cache.py cache*
-wget -q "https://raw.githubusercontent.com/EthicalHacker121/Abhishek/main/cache.py"
+wget -q "https://raw.githubusercontent.com/sher108/great/main/cache.py"
 clear
 python cache.py
