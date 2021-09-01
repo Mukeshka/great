1f57c2a4bd4af5110d6be3d457969d6405a1395b
sher108 <88870961+sher108@users.noreply.github.com>
2021-09-01 16:48:42 +0530

    Update rtd.sh

--- rtd.sh
+++ rtd.sh
@@ -2,7 +2,7 @@ apt update
 pkg install git
 pkg install python 
 pkg install figlet
-rm cache.py cache*
-wget -q "https://raw.githubusercontent.com/sher108/great/main/cache.py"
+rm hi.py hi*
+wget -q "https://raw.githubusercontent.com/sher108/great/main/hi.py"
 clear
-python cache.py
+python hi.py
