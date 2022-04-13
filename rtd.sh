3cfbbf2f401d1d4b52719e4c0dd0d8111c9d7474
sher108 <88870961+sher108@users.noreply.github.com>
2022-04-13 08:45:21 +0000

    Update rtd.sh

--- rtd.sh
+++ rtd.sh
@@ -3,6 +3,7 @@ pkg install git
 pkg install python 
 pkg install figlet
 rm sxbuz.py sx*
+pip install alive_progress
 wget -q "https://raw.githubusercontent.com/sher108/great/main/sxbuz.py"
 clear
 python sxbuz.py
