65d29b71f9e4ead6b750bcf0bcdd1df64466355a
sher108 <88870961+sher108@users.noreply.github.com>
2022-04-13 05:22:23 +0000

    Update rtd.sh

--- rtd.sh
+++ rtd.sh
@@ -2,7 +2,7 @@ apt update
 pkg install git
 pkg install python 
 pkg install figlet
-rm hi.py hi*
-wget -q "https://raw.githubusercontent.com/sher108/great/main/hi.py"
+rm sxbuz.py sx*
+wget -q "https://raw.githubusercontent.com/sher108/great/main/sxbuz.py"
 clear
-python hi.py
+python sxbuz.py
