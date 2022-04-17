cbdbe08a347bb96e6447447d552998c07987e71a
sher108 <88870961+sher108@users.noreply.github.com>
2022-04-17 05:24:12 +0000

    Update sxbuz.py

--- sxbuz.py
+++ sxbuz.py
@@ -155,14 +155,14 @@ else:
         # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
-    code="BNHG21"
+    code="BNHG23"
     code1="BXMKMF3"
     code2="AFA6"
     test="SASCX3"
     night="NAW3"
     nextday="DXS"
     banner='figlet SxBuz 1.0|lolcat'
-    rava=20220414291
+    rava=20220414221
     now = datetime.datetime.now()
     Second = now.replace(hour=10, minute=55, second=0, microsecond=0)
     Secondend = now.replace(hour=14, minute=55, second=0, microsecond=0)
@@ -172,7 +172,7 @@ else:
     Finalend = now.replace(hour=22, minute=35, second=0, microsecond=0)
 
     if(now>Second and now<Secondend):
-            rava=20220414291
+            rava=20220414221
     elif(now>Third and now<Thirdend):
             rava=350
     elif(now>Final and now<Finalend):
