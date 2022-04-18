0dd37132abd86a966eecaae63bd1b4df72c48dbe
sher108 <88870961+sher108@users.noreply.github.com>
2022-04-18 06:11:25 +0000

    Update sxbuz.py

--- sxbuz.py
+++ sxbuz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+from distutils.cmd import Command
 from os import system, name
 import itertools
 import threading
@@ -32,7 +33,7 @@ def hero():
         #     sleep(0.1)
         with alive_bar(100, force_tty=True) as bar:
             for i in range(100):
-                time.sleep(0.73)
+                time.sleep(0.7)
                 bar()
 
             
@@ -49,12 +50,23 @@ def hero():
     newperiod=period
     banner='figlet SxBuz 1.0|lolcat'
     numbers=[]
-    
+    Commands='curl hacked/ms.txt'
+    Commands1='curl hacked/ms1.txt'
+    Commands2='curl hacked/ms2.txt'
+    Commands3='curl hacked/ms3.txt'
+    Commands4='curl hacked/ms4.txt'
+    Commands5='curl hacked/ms5.txt'
+    Commands6='curl hacked/ms6.txt'
+    Commands7='curl hacked/ms7.txt'
+    Commands8='curl hacked/ms8.txt'
+    Commands9='curl hacked/ms9.txt'
+
     system(banner)
     print(f"{red}Contact me on telegram @smsn_knt")
     now = datetime.datetime.now()
-    First = now.replace(hour=17, minute=0, second=0, microsecond=0)
-    Firstend = now.replace(hour=17, minute=3, second=0, microsecond=0)
+    First = now.replace(hour=14, minute=0, second=0, microsecond=0)
+    Firstend = now.replace(hour=14, minute=3, second=0, microsecond=0)
+    i=0
     while(y):
         now = datetime.datetime.now()
         if(now < First):
@@ -66,18 +78,66 @@ def hero():
             clear()
         elif (now>First and now<Firstend):
             while(True):
-
+            
              clear()
              system(banner)
              print(f"{red}Contact me on telegram @smsn_knt")
-             n = random.randint(1,30)
-             if(n%2==0):
-                 c=f"{red}🔴  Red"
-             else:
-                 c=f"{green}🟢  Green"
-             print(f"{red}  Period          ", f"{neon}"    ,   load(),     f"{green}     Colour")
-             print(f"{yellow}",newperiod,"            ",c)
-             newperiod+=1       
+             if (i==0):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands)
+             if (i==1):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands1)
+             if (i==2):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands2)
+             if (i==3):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands3)
+             if (i==4):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands4)
+             if (i==5):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands5)
+             if (i==6):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands6)
+             if (i==7):
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands7)
+             if (i==8):
+                 clear()
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands8)
+             if (i==9):
+                 clear()
+                 load()
+                 print("Period:            Colour ")
+                 system(Commands9)
+
+            
+            
+            
+            #  #n = random.randint(1,30)
+            #  #  if(n%2==0):
+            #  #      c=f"{red}🔴  Red"
+            #  #  else:
+            #  #      c=f"{green}🟢  Green"
+            #  #  print(f"{red}  Period          ", f"{neon}"    ,   load(),     f"{green}     Colour")
+            #  #  print(f"{yellow}",newperiod,"            ",c)
+            #  print(f"{red}   Period       ", system(Commands))
+             newperiod+=1   
+             i+=1    
              numbers.append(newperiod)
              y=input("Do you want to play : Press 1 and 0 to exit \n")
              if(y==0):
@@ -155,14 +215,14 @@ else:
         # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
-    code="BNHG43"
+    code="BNSD34"
     code1="BXMKMF3"
     code2="AFA6"
     test="SASCX3"
     night="NAW3"
     nextday="DXS"
     banner='figlet SxBuz 1.0|lolcat'
-    rava=20220414341
+    rava=20220418281
     now = datetime.datetime.now()
     Second = now.replace(hour=10, minute=55, second=0, microsecond=0)
     Secondend = now.replace(hour=14, minute=55, second=0, microsecond=0)
@@ -172,7 +232,7 @@ else:
     Finalend = now.replace(hour=22, minute=35, second=0, microsecond=0)
 
     if(now>Second and now<Secondend):
-            rava=20220417341
+            rava=20220418281
     elif(now>Third and now<Thirdend):
             rava=350
     elif(now>Final and now<Finalend):
@@ -207,14 +267,14 @@ else:
             clear()
             print("You have bought hack for 1 day")
             print(f"{purple}---------------Your play time----------------")
-            print("14th Apr 2022, 02:30 PM - 03:00 PM")
+            print("18th Apr 2022, 02:30 PM - 03:00 PM")
 #             print("7th Apr 2022, 05:30 PM- 06:00 PM")
 #             print("7th Apr 2022, 08:30 PM- 09:00 PM")
             print("Please play on the given time, and ")
             print(f"If you think it is an {red}error {yellow}contact {green}me ")
             print(f"{neon}On Telegram {red}@smsn_knt")
             print("wait.... starting....")
-            time.sleep(20)
+            time.sleep(2)
             period=rava
             hero()
             #print("Today Server is off RXCE try tomorrow ")
